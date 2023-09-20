ec31b6700c309b28eb1788a969bfddb31c697480
patchpark <kevinminsang99@gmail.com>
2023-09-20 20:30:03 +0900

    Week1 과제 제출

---
+++ Week1/GDSC__1st_HW/src/org/example/Cat.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cat extends Animal{
+
+	@Override
+	public void speak() {
+		System.out.println("cat says Meow!");
+		
+	}
+	
+}
---
+++ Week1/GDSC__1st_HW/src/org/example/Dog.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Dog extends Animal{
+
+	@Override
+	public void speak() {
+		System.out.println("dog says Woof!");
+		
+	}
+
+	
+}
